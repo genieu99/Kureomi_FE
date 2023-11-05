bea1b88a53fa1b2bab530169ee0efc851e0506b6
yunssup <btob031541@gmail.com>
2023-11-06 00:19:33 +0900

    feat : 도입 페이지

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Start from "./pages/Start/Start";
+
+function App() {
+  return (
+    <div className="App">
+      <Start />
+    </div>
+  );
+}
+
+export default App;
