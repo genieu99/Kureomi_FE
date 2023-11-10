05c72e42447326a6d53823a0eb07d56cadd0bcf0
Hyesoo <kimhyesu914@gmail.com>
2023-11-10 22:12:59 +0900

    feat: 홈페이지 api연결 완료

--- src/pages/Home/UserHome.jsx
+++ src/pages/Home/UserHome.jsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from "react";
 import {BackGround,BackIMG,Title} from "../Login/Style"
-import {Text,LinkBox,CopyBtn,ClickBox,PresentComponent,FlexBox} from "./Style";
+import {Text,LinkBox,CopyBtn,ClickBox,PresentComponent,FlexBox,PresentBox} from "./Style";
 import { Link, useLocation, useParams} from "react-router-dom";
 import axios from "axios";
 
@@ -10,7 +10,7 @@ function Home(){
   const uniqueUrl=location.pathname; // /Home/cf093be6-17e3-49c9-8e7b-f77a30eac2d4
 
   const[ShowPresent,setShowPresent]=useState(false);
-  const[ShowClickPresent,setShowClickPresent]=useState(true);
+  const[ShowClickPresent,setShowClickPresent]=useState([]);
 
   const userLocation="http://localhost:5173"+uniqueUrl;
   console.log(uniqueUrl);
@@ -19,10 +19,17 @@ function Home(){
     alert("복사 되었습니다!")
   }
   const [response, setResponse] = useState(null);
+  const [photoresponse, setPhotoResponse] = useState(null);
+
   const [userName,setUserName]=useState("");
   const apiUrl="/api/v1/kureomi"+uniqueUrl;
-  console.log(apiUrl);
 
+  const uniqueId = location.pathname.replace("/home/", ""); //뒤에 uniqueid만 나오도록
+
+  const photoapiUrl="/api/v1/kureomi/"+uniqueId;
+  
+
+  //유저 이름 가져오기
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -40,21 +47,39 @@ function Home(){
     if(response) {
       setUserName(response.userName);
     }
-  }, [response]); // response가 변경될 때마다 이 useEffect는 실행됩니다.
+  }, [response]); 
+
+  //포토꾸러미 가져오기
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const photoresponse = await axios.get(photoapiUrl,{ withCredentials: true });
+        setPhotoResponse(photoresponse.data);
+      } catch (error) {
+        console.error('오류', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const size=photoresponse?.length;
 
   return(
     <BackGround>
       <BackIMG imgUrl="/Img/Userhome.png" opaCity="1.0">
         <Title marginTop="70px">{userName}의 홈</Title>
-        <Text>받은 꾸러미 : N개</Text>
+        <Text>받은 꾸러미 : {size}개</Text>
         <FlexBox height="10%" flag="center">
           <LinkBox>{userLocation}</LinkBox><CopyBtn onClick={handleCopy}>복사</CopyBtn>
         </FlexBox>
-        <FlexBox height="45%" margin="0 0 0 10%">
-          {ShowPresent && <PresentComponent />}
-        </FlexBox>
+        <PresentBox>
+        {ShowPresent && Array.from({ length: size }, (_, index) => (
+          <PresentComponent key={index} />
+          ))}
+        </PresentBox>
         <FlexBox height="22%" flag="center">
-          {ShowClickPresent && <ClickBox onClick={() => 
+          {ShowClickPresent &&<ClickBox onClick={() => 
             {setShowPresent(true); setShowClickPresent(false);}}>
             </ClickBox> } 
         </FlexBox>
