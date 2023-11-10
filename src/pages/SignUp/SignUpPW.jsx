884272f1ad35f6bfb4fe4fe83838a3c5c66183fa
yunssup <btob031541@gmail.com>
2023-11-10 20:01:05 +0900

    visitsend api 연결

--- src/pages/SignUp/SignUpPW.jsx
+++ src/pages/SignUp/SignUpPW.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Container, Title, SubTitle, ButtonNext, Input } from "./Style";
-import { Link,useLocation,useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { BackGround } from "../Login/Style";
 import axios from "axios";
 
@@ -11,14 +11,14 @@ function Start() {
     setPassword(event.target.value); // 입력된 비밀번호를 상태 변수에 업데이트
   };
 
-  const location=useLocation();
-  const userName=location.state.userName;
-  const email=location.state.email;
+  const location = useLocation();
+  const userName = location.state.userName;
+  const email = location.state.email;
   console.log(userName);
   console.log(email);
   console.log(password);
-  const navigate=useNavigate();
- 
+  const navigate = useNavigate();
+
   const isFormValid = () => {
     // 유효성 검사 로직 추가
     return (
@@ -26,7 +26,7 @@ function Start() {
     );
   };
 
-const handleNextClick = async () => {
+  const handleNextClick = async () => {
     if (!isFormValid()) {
       // 유효하지 않은 경우 처리
       alert("모든 필수 입력란을 채워주세요.");
@@ -34,35 +34,34 @@ const handleNextClick = async () => {
     }
 
     const userData = {
-      userName:userName,
-      email:email,
-      password:password,
+      userName: userName,
+      email: email,
+      password: password,
     };
-    
+
     console.log(userData);
 
     try {
       const response = await axios.post(
         "/api/v1/kureomi/signup",
         {
-          userName:userName,
-          email:email,
-          password:password,
+          userName: userName,
+          email: email,
+          password: password,
         },
-        {withCredentials: true}
+        { withCredentials: true }
       );
-      
+
       console(response.status);
-      
-      if (response.status ===201) {
+
+      if (response.status === 201) {
         console.log("회원가입이 완료되었습니다.");
         console.log("userName:", userName);
         console.log("Email:", email);
         console.log("Password:", password);
         setSignUpComplete(true); // 회원가입 완료 상태를 설정
         console.log(response.data);
-      } 
-      else {
+      } else {
         console.log("회원가입 실패");
       }
     } catch (error) {
@@ -83,11 +82,13 @@ const handleNextClick = async () => {
           onChange={handlePasswordChange}
         />
 
-<input type="submit" onClick={async () => {
-  await handleNextClick();
-  navigate("/SignUpSucess");
-}}>
-
+        <input
+          type="submit"
+          onClick={async () => {
+            await handleNextClick();
+            navigate("/SignUpSucess");
+          }}
+        >
           {/* <Link to="/SignUpSucess">확인</Link>{" "} */}
         </input>
       </Container>
--- src/pages/Visit/VisitWrite.jsx
+++ src/pages/Visit/VisitWrite.jsx
@@ -2,33 +2,68 @@ import React, { useState } from "react";
 import { Container, Title, ButtonSigns, Input, ButtonSend } from "./Style";
 import { Link } from "react-router-dom";
 import { BackGround } from "../Login/Style";
+import axios from "axios";
 
 function Start() {
-  const [name, setName] = useState(""); // 이름을 추적하는 상태 변수
-  const [letter, setLetter] = useState(""); // 편지 내용을 추적하는 상태 변수
+  const [name, setName] = useState("");
+  const [letter, setLetter] = useState("");
+  const [writeSendComplete, setWriteSendComplete] = useState(false);
 
   const handleNameChange = (event) => {
-    setName(event.target.value); // 이름 입력을 상태 변수에 업데이트
+    setName(event.target.value);
   };
 
   const handleLetterChange = (event) => {
-    setLetter(event.target.value); // 편지 내용을 상태 변수에 업데이트
+    setLetter(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // 폼 제출 기본 동작 방지
+
+    const userData = {
+      writer: name, // 변수명 수정
+      message: letter, // 변수명 수정
+      photoIdList: [], // 아직 photoIdList가 어디에서 왔는지는 코드에서 확인되지 않았습니다.
+    };
+
+    console.log(userData);
+
+    try {
+      const response = await axios.post(
+        "/api/v1/kureomi/{uniqueUrl}/create",
+        userData, // 수정된 부분
+        { withCredentials: true }
+      );
+
+      console.log(response.status);
+
+      if (response.status === 201) {
+        console.log("전송 완료");
+        setWriteSendComplete(true);
+        console.log(response.data);
+      } else {
+        console.log("전송 실패");
+      }
+    } catch (error) {
+      console.log("오류 발생 : ", error);
+    }
   };
 
   const textareaStyle = {
     width: "100%",
     maxWidth: "80%",
-    height: "200px" /* 원하는 높이로 설정하세요 */,
+    height: "200px",
     padding: "10px",
     border: "1px solid #ccc",
     borderRadius: "4px",
     fontSize: "16px",
-    resize: "vertical" /* 사용자가 수직으로만 리사이즈 가능하도록 설정 */,
+    resize: "vertical",
     background: "rgba(255, 255, 255, 0.50)",
     outline: "none",
     margin: "30px",
     fontfamily: "SUITE",
   };
+
   return (
     <BackGround>
       <Container>
@@ -37,21 +72,23 @@ function Start() {
           <Link to="/SignUpName">회원가입</Link>{" "}
         </ButtonSigns>
         <Title>김지은님의 홈</Title>
-        <Input
-          type="text"
-          placeholder="이름을 알려주세요! (닉네임도 좋아요)"
-          value={name}
-          onChange={handleNameChange}
-        />
-        <textarea
-          style={textareaStyle}
-          placeholder="편지를 입력해주세요"
-          value={letter}
-          onChange={handleLetterChange}
-        />
-        <ButtonSend>
-          <Link to="/VisitSend">보내기</Link>{" "}
-        </ButtonSend>
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            placeholder="이름을 알려주세요! (닉네임도 좋아요)"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <textarea
+            style={textareaStyle}
+            placeholder="편지를 입력해주세요"
+            value={letter}
+            onChange={handleLetterChange}
+          />
+          <ButtonSend type="submit">
+            <Link to="/VisitSend">보내기</Link>{" "}
+          </ButtonSend>
+        </form>
       </Container>
     </BackGround>
   );
